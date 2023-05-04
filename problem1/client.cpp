28bc8cbf7532d962f0835948ecbb44d4df1d75fd
kjungw1025 <kjungw1025@naver.com>
2023-05-04 20:28:34 +0900

    feat: Modifying code to dynamic assignment
    Use vector and string
    For long strings, split into 1024 sizes in client and transfer to server

--- problem1/client.cpp
+++ problem1/client.cpp
@@ -3,13 +3,16 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main()
 {
     int cs;
-    char buf[5];
-    int recvbuf[5];
+    string buf; // client가 입력한 문자열을 담은 변수
+    int buf_size[1]; // client가 입력한 문자열의 길이를 담은 변수
+    vector<int> recvbuf;
     struct sockaddr_in csa;
 
     memset(&csa, 0, sizeof(csa));
@@ -21,15 +24,45 @@ int main()
     connect(cs, (struct sockaddr *) &csa, sizeof(csa));
 
     cout << "Input String : ";
-    cin >> buf;
-    send(cs, buf, 5, 0);
+    getline(cin, buf);
 
-    recv(cs, recvbuf, sizeof(recvbuf) / sizeof(char), 0);
+    buf_size[0] = buf.size();
+    send(cs, buf_size, sizeof(buf_size), 0); // server에, client가 입력한 문자열의 길이를 우선적으로 전송
+
+    if (buf.length() / 1024 > 0) {  // 문자열의 길이가 1024보다 크다면
+        int quotient = buf.length() / 1024;
+        int remainder = buf.length() % 1024;
+
+        string tmp;
+        int start_idx, end_idx;
+        for (int i = 0; i < quotient; i++) {
+            start_idx = 1024 * i;
+            end_idx = 1024 * (i + 1) - 1;
+            tmp = buf.substr(start_idx, end_idx + 1);
+            send(cs, tmp.c_str(), tmp.length(), 0); // 문자열을 1024씩 쪼개서 server에 전송
+        }
+
+        tmp = buf.substr(end_idx + 1, end_idx + 1 + remainder + 1);
+        send(cs, tmp.c_str(), tmp.length(), 0);
+    }
+    else    // 문자열의 길이가 1024보다 작다면, 문자열 그대로 server에 전송
+        send(cs, buf.c_str(), buf.length(), 0);
+
+    recvbuf.resize(buf.length());
+    recv(cs, recvbuf.data(), buf.length() * sizeof(int), 0); // server가 보낸 ASCII Code 결과 값들을 받음
+
+    // 출력 부분
     cout << "Receive ";
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < recvbuf.size(); i++) {
         cout << recvbuf[i];
     }
-    cout << '\n';
+    cout << "\n\n";
+
+    cout << "Detail\n";
+    for (int i = 0; i < recvbuf.size(); i++) {
+        cout << buf[i] << ": " << recvbuf[i] << "\n";
+    }
+    cout << "\n";
 
     close(cs);
 }
