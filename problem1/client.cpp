18d5aa4815dac848c59dc0eebfac2e3727d5a3d8
kjungw1025 <kjungw1025@naver.com>
2023-05-18 09:29:17 +0900

    feat: Modifying code

--- problem1/client.cpp
+++ problem1/client.cpp
@@ -29,24 +29,7 @@ int main()
     buf_size[0] = buf.size();
     send(cs, buf_size, sizeof(buf_size), 0); // server에, client가 입력한 문자열의 길이를 우선적으로 전송
 
-    if (buf.length() / 1024 > 0) {  // 문자열의 길이가 1024보다 크다면
-        int quotient = buf.length() / 1024;
-        int remainder = buf.length() % 1024;
-
-        string tmp;
-        int start_idx, end_idx;
-        for (int i = 0; i < quotient; i++) {
-            start_idx = 1024 * i;
-            end_idx = 1024 * (i + 1) - 1;
-            tmp = buf.substr(start_idx, end_idx + 1);
-            send(cs, tmp.c_str(), tmp.length(), 0); // 문자열을 1024씩 쪼개서 server에 전송
-        }
-
-        tmp = buf.substr(end_idx + 1, end_idx + 1 + remainder + 1);
-        send(cs, tmp.c_str(), tmp.length(), 0);
-    }
-    else    // 문자열의 길이가 1024보다 작다면, 문자열 그대로 server에 전송
-        send(cs, buf.c_str(), buf.length(), 0);
+    send(cs, buf.c_str(), buf.length(), 0);
 
     recvbuf.resize(buf.length());
     recv(cs, recvbuf.data(), buf.length() * sizeof(int), 0); // server가 보낸 ASCII Code 결과 값들을 받음
